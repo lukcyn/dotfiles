bda6b669539053f9d4ce6b57317d41a6d11d4c7d
lukcyn <lukaszcyndzerr@gmail.com>
2026-08-02 21:03:12 +0200

    Added node and npm

--- 01-system.sh
+++ 01-system.sh
@@ -19,7 +19,15 @@ sudo pacman -S --needed --noconfirm \
   bash-completion
 
 # Dir setup
-sudo pacman -S xdg-user-dirs
+sudo pacman -S --needed --noconfirm xdg-user-dirs
+
+if [ ! -f "$HOME/.config/user-dirs.dirs" ]; then
+  echo "Setting up user dirs..."
+  xdg-user-dirs-update
+else
+  echo "User dirs already in place. Skipping..."
+fi
+
 xdg-user-dirs-update
 
 mkdir -p ${HOME}/dev/repo
@@ -28,8 +36,13 @@ mkdir -p ${HOME}/dev/project
 # yay
 cd ${HOME}/dev/repo
 
-if [ ! -d "$HOME/yay" ]; then
-  git clone https://aur.archlinux.org/yay.git yay
+if ! command -v yay >/dev/null 2>&1; then
+  if [ ! -d "$HOME/yay" ]; then
+    git clone https://aur.archlinux.org/yay.git "$HOME/yay"
+  fi
+
+  cd "$HOME/yay" && makepkg -si
 fi
 
-cd yay && makepkg -si
+# Node
+sudo pacman -S --needed --noconfirm nodejs npm
